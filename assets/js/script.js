648a0e2854e97b3b5cd75d64245c80dbdb4c6ce2
Aaliyah <aaliyah.symlar@vanderbilt.edu>
2021-08-06 16:26:11 -0500

    Made changes to nav and links. added manifest.json file along with dependencies and icons.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,4 +4,30 @@ require("boostrap");
 
 $(document).ready(function() {
     // carousel or just a list of shoes with images
-})
+
+    const carouselSlides = [
+        {
+            title: "Nikes that are now on Sale for a great price!",
+            btnText: "View Shoes Here",
+            btnUrl: "shoes.html"
+        }
+    ]
+
+    carouselSlides.forEach((slide, i) => {
+        $('.carousel-inner').append(`
+        <div class="carousel-item fullscreen-carousel" style="background-image: url('${slide.img}')">
+          <div class="d-flex h-100 align-items-center justify-content-center carousel-caption">
+              <div class="container">
+                <div class="row align-items-center justify-content-center">
+                    <h2 class="display-4 mb-2">${slide.title}</h2>
+                </div>
+                <div class=" mt-4 row align-items-center justify-content-center"> 
+                  <a class="btn btn-primary" href="${slide.btnUrl}">
+                      ${slide.btnText}
+                  </a>
+                </div>
+              </div>
+          </div>
+        </div>`)
+    })
+});
